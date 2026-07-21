b101decf972b874254fbdbcb41fed2dabd93baeb
Riza <mysteriza@proton.me>
2026-07-21 20:58:36 +0700

    Add GPU WPA cracking with hashcat

--- src/config.py
+++ src/config.py
@@ -1,6 +1,16 @@
+import os
+import platform
+
 WORDLIST_URL = "https://raw.githubusercontent.com/Mysteriza/WiFi-Password-Wordlist/main/wifite.txt"
 AIRCRACK_WIN_URL = "https://download.aircrack-ng.org/aircrack-ng-1.7-win.zip"
 HANDSHAKES_DIR = "handshakes"
 RESULTS_DIR = "cracked_results"
 WORDLIST_NAME = "wifite.txt"
 BIN_DIR = "bin"
+
+HASHCAT_VERSION = "6.2.6"
+HASHCAT_WIN_URL = f"https://hashcat.net/files/hashcat-{HASHCAT_VERSION}.7z"
+HASHCAT_LINUX_URL = f"https://hashcat.net/files/hashcat-{HASHCAT_VERSION}.tar.xz"
+HASHCAT_DIR = os.path.join(BIN_DIR, f"hashcat-{HASHCAT_VERSION}")
+HASHCAT_EXE = "hashcat.exe" if platform.system() == "Windows" else "hashcat"
+HCOV_DIR = "hc22000_cache"
