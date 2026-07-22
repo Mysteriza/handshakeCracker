56173b52b6d24b211509d184b2f2d407d725cf95
Riza <mysteriza@proton.me>
2026-07-22 11:30:12 +0700

    Add auto-update via git pull on startup

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ from src.cracker import get_already_cracked_essids, crack_handshake
 from src.setup import auto_setup
 from src.gpu import has_discrete_gpu, get_gpu_name
 from src.hashcat_cracker import hashcat_crack_handshake, ensure_hashcat
+from src.updater import check_for_updates
 
 
 class PcapValidator(Validator):
@@ -110,6 +111,9 @@ def get_manual_handshake_paths(session: PromptSession) -> list[str]:
 SEPARATOR = "-" * 60
 
 def main():
+    if check_for_updates():
+        sys.exit(0)
+
     os.system("cls" if os.name == "nt" else "clear")
 
     try:
